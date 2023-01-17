e10c95fc2fb7bafca1c12a1ee59ddfff094ab31e
Ruben Vazquez <ruben.vazquez@gds.ey.com>
2023-01-16 19:24:47 -0600

    last changes for deployment

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,3 +1,4 @@
+ 
 const githubReducer = (state, action) =>{
     switch(action.type){
         case 'GET_USERS':
@@ -5,6 +6,13 @@ const githubReducer = (state, action) =>{
                 ...state,
                 users: action.payload,
                 loading: false               
+            }
+        case 'GET_USER_AND_REPOS':
+            return {
+                ...state,
+                user: action.payload.user,
+                repos: action.payload.repos,
+                loading: false,
             }
         case "GET_USER":
             return {
@@ -15,7 +23,7 @@ const githubReducer = (state, action) =>{
         case "SET_LOADING":
             return{
                 ...state,
-                loading: false
+                loading: true
             }
         
         case "CLEAR_USERS":
